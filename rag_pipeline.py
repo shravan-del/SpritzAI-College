883445681986407efc50462df4a7c85b7092006c
Rohan Magesh <60275372+rmagesh-quml@users.noreply.github.com>
2025-03-28 19:56:19 -0400

    adding os capabilities

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,10 +1,11 @@
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from retriever import get_retriever
+import os
 
 def get_answer(query):
     qa = RetrievalQA.from_chain_type(
-        llm=ChatOpenAI(temperature=0),
+        llm=ChatOpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"), temperature=0),
         retriever=get_retriever()
     )
     return qa.run(query)
